1deb5063562aa6c28bc180adab521fe5dbbbe9fe
James Morris <jamez@hey.com>
2022-12-19 19:48:35 +0000

    date dot spacing

---
+++ src/lib/ui/tweet/TweetUserName.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let username: string | undefined = undefined;
+</script>
+
+<a href={`/user/${username}`} class="truncate text-light-secondary text-sm" tabIndex={-1}>
+	@{username}
+</a>
