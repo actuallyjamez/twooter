d342c5e79782935f12acfea0cd1664555a7a41cb
James Morris <jamez@hey.com>
2022-12-22 20:52:25 +0000

    improve icon button

---
+++ src/lib/ui/tweet/IconButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import clsx from "clsx";
+
+	let className: string = "";
+	export { className as class };
+</script>
+
+<div class="inline-flex relative w-[17.5px] h-[17.5px]">
+	<div
+		class={clsx(
+			"absolute m-[-8px] inset-0 opacity-10 rounded-full transition-colors",
+			className
+		)}
+	/>
+	<slot />
+</div>
