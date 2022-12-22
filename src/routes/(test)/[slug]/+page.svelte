42d76e804ec30464903f084369903e9497851a1c
James Morris <jamez@hey.com>
2022-12-22 18:23:02 +0000

    more cleanup

--- src/routes/(test)/[slug]/+page.svelte
+++ src/routes/(test)/[slug]/+page.svelte
@@ -4,5 +4,7 @@
 	// export let data: PageData;
 </script>
 
-this is a user page for a user named "user" and the data is passed in as a prop. the data is fetched
-in the route handler and passed in as a prop to the page component.
+<p>
+	this is a user page for a user named "user" and the data is passed in as a prop. the data is
+	fetched in the route handler and passed in as a prop to the page component.
+</p>
